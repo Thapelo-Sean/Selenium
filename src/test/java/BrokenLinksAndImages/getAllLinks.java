2674711e44c249c07224e439016feb84d304840b
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-13 10:03:55 +0200

    Modified getAllLinks class

--- src/test/java/BrokenLinksAndImages/getAllLinks.java
+++ src/test/java/BrokenLinksAndImages/getAllLinks.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class getAllLinks {
     public static WebDriver driver;
@@ -14,20 +15,26 @@ public class getAllLinks {
     //Get all links
     public static void main(String[] args)
     {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get("https://demoqa.com/links");
+        try{
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.get("https://demoqa.com/links");
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
-        String url = "";
-        List<WebElement> allUrls = driver.findElements(By.tagName("a"));
-        System.out.println("Number of URL's in the page are " + allUrls.size());
+            String url = "";
+            List<WebElement> allUrls = driver.findElements(By.tagName("a"));
+            System.out.println("Number of URL's in the page are " + allUrls.size());
 
-        Iterator <WebElement> iterator = allUrls.listIterator();
-        while (iterator.hasNext())
-        {
+            Iterator <WebElement> iterator = allUrls.listIterator();
+            while (iterator.hasNext())
+            {
                 url = iterator.next().getText();
                 System.out.println(url);
-                driver.quit();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        driver.close();
     }
 }
