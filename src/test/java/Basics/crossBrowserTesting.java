f43b726d981b1adb54e2e81d71389cfdd1861e9d
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-13 10:28:17 +0200

    Modified dragAndDrop class

--- src/test/java/Basics/crossBrowserTesting.java
+++ src/test/java/Basics/crossBrowserTesting.java
@@ -2,6 +2,7 @@ package Basics;
 
 import java.util.concurrent.TimeUnit;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,45 +13,52 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class crossBrowserTesting {
-	
+
 	WebDriver driver;
-	
+
 	@BeforeTest
 	@Parameters("browser")
-	public void setup(String browser) throws Exception 
+	public void setup(String browser) throws Exception
+
 	{
-		if(browser.equalsIgnoreCase("firefox")) 
+		if(browser.equalsIgnoreCase("firefox"))
 		{
 			System.setProperty("webdriver.gecko.driver", "location of the driver");
 			driver = new FirefoxDriver();
 		}
-		
+
 		 else if(browser.equalsIgnoreCase("chrome"))
 		{
-			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Katan\\OneDrive\\Desktop\\MyAutomation\\Selenium\\chromedriver.exe");
+			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 		}
-		
-		else if(browser.equalsIgnoreCase("edge")) 
+
+		else if(browser.equalsIgnoreCase("edge"))
 		{
 			System.setProperty("webdriver.egde,driver", "location of the driver");
 			driver = new EdgeDriver();
 		}
-		
-		else 
+
+		else
 		{
 			throw new Exception ("Browser is not correct");
 		}
-		
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
 	}
-	
+
 	@Test
-	public void testParameterWithXml() throws InterruptedException 
+	public void testParameterWithXml() throws InterruptedException
 	{
-		driver.get("https://www.google.com");
-		driver.findElement(By.name("q")).sendKeys("Selenium");
-		driver.findElement(By.name("btnK")).click();
-		driver.quit();
+		try
+		{
+			driver.get("https://www.google.com");
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			driver.findElement(By.name("q")).sendKeys("Selenium");
+			driver.findElement(By.name("btnK")).click();
+			driver.quit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
