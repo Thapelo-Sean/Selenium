4fd576efe569ad45fe368451427a64bb07c412be
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-13 10:53:46 +0200

    Modified HandlingIframes class

--- src/test/java/Basics/HandlingIframes.java
+++ src/test/java/Basics/HandlingIframes.java
@@ -1,5 +1,6 @@
 package Basics;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,22 +13,28 @@ public class HandlingIframes {
 
     public static void main(String [] arg0)
     {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Katan\\OneDrive\\Desktop\\MyAutomation\\Selenium\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get(" https://demoqa.com/frames ");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        try
+        {
+            WebDriverManager.chromedriver().setup();
+            WebDriver driver = new ChromeDriver();
+            driver.get(" https://demoqa.com/frames ");
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        List<WebElement> iframes = driver.findElements(By.tagName("iframe"));
-        int numberOfFrames = iframes.size();
-        System.out.println("Iframes present is this webpage are" + iframes);
-        System.out.println("Total number of iframes are " + numberOfFrames);
+            List<WebElement> iframes = driver.findElements(By.tagName("iframe"));
+            int numberOfFrames = iframes.size();
+            System.out.println("Iframes present is this webpage are" + iframes);
+            System.out.println("Total number of iframes are " + numberOfFrames);
 
-        //switching to iframes
+            //switching to iframes
 
-        //driver.switchTo().frame("frame1").getTitle(); //by id
-        System.out.println("The title of the iframe is " + driver.switchTo().frame("frame1").getTitle());
-        System.out.println("Main Frame is " +  driver.switchTo().defaultContent().getTitle());
-        driver.close();
+            driver.switchTo().frame("frame1").getTitle(); //by id
+            System.out.println("The title of the iframe is " + driver.switchTo().frame("frame1").getTitle());
+            System.out.println("Main Frame is " +  driver.switchTo().defaultContent().getTitle());
+            driver.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Execution completed");
     }
 }
