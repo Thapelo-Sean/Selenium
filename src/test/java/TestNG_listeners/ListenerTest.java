ca06cc5b8b90c740700dccca6e6ca2cb40d79130
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-13 11:37:25 +0200

    Modified TestNG listener class

--- src/test/java/TestNG_listeners/ListenerTest.java
+++ src/test/java/TestNG_listeners/ListenerTest.java
@@ -4,7 +4,6 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-
 public class ListenerTest implements ITestListener {
 
 	public void onTestStart(ITestResult Result)
@@ -20,7 +19,5 @@ public class ListenerTest implements ITestListener {
 	public void onTestSuccess(ITestResult Result)
 	{
 		System.out.println("Testcase :" + Result.getName() + " passed");
-		
-	} 
-	
+	}
 }
--- src/test/java/TestNG_listeners/TestCase.java
+++ src/test/java/TestNG_listeners/TestCase.java
@@ -1,6 +1,6 @@
 package TestNG_listeners;
 
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -10,27 +10,30 @@ import org.testng.annotations.Test;
 @Listeners(ListenerTest.class)
 
 public class TestCase {
-
 	@Test
 	public void main() throws InterruptedException {
-		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Katan\\OneDrive\\Desktop\\MyAutomation\\Selenium\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		
-		driver.get("https://www.edureka.co/");
-		driver.manage().window().maximize();
-		JavascriptExecutor js = (JavascriptExecutor)driver;
-		driver.findElement(By.linkText("Cloud Computing")).click();
-		js.executeScript("window.scrollBy(0, 500)");
-		Thread.sleep(1000);
-		js.executeScript("scrollBy(0, 1000)");
-		Thread.sleep(1000);
-		js.executeScript("scrollBy(0, 1000)");
-		Thread.sleep(1000);
-		js.executeScript("scrollBy(0, 500)");
-		Thread.sleep(1000);
-		driver.findElement(By.className("trackButton")).click();
-		Thread.sleep(1000);
-		driver.quit();
+		try
+		{
+			WebDriverManager.chromedriver().setup();
+			WebDriver driver = new ChromeDriver();
+
+			driver.get("https://www.edureka.co/");
+			driver.manage().window().maximize();
+			JavascriptExecutor js = (JavascriptExecutor)driver;
+			driver.findElement(By.linkText("Cloud Computing")).click();
+			js.executeScript("window.scrollBy(0, 500)");
+			Thread.sleep(1000);
+			js.executeScript("scrollBy(0, 1000)");
+			Thread.sleep(1000);
+			js.executeScript("scrollBy(0, 1000)");
+			Thread.sleep(1000);
+			js.executeScript("scrollBy(0, 500)");
+			Thread.sleep(1000);
+			driver.findElement(By.className("trackButton")).click();
+			Thread.sleep(1000);
+			driver.quit();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
