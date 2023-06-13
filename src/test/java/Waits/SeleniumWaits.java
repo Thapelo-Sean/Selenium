0ff1e9cabcdbeef4cf6cca01c8dbbc6c97d5d2b6
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-13 11:50:45 +0200

    Modified SeleniumWaits class

--- src/test/java/Waits/SeleniumWaits.java
+++ src/test/java/Waits/SeleniumWaits.java
@@ -2,6 +2,7 @@ package Waits;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,26 +15,34 @@ public class SeleniumWaits {
 	public static WebDriver driver;
 
 	public static void main(String[] args) throws InterruptedException {
-		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Katan\\OneDrive\\Desktop\\MyAutomation\\Selenium\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("https://www.facebook.com");
-		
-		WebElement firstname = driver.findElement(By.xpath("//*[@id=\"email\"]"));
-		WebElement password = driver.findElement(By.xpath("//*[@id=\"passContainer\"]"));
-		WebElement forgotAccount  = driver.findElement(By.xpath("//*[@id=\"u_0_2_7P\"]/div[3]/a"));
-		
-		sendKeys(driver, firstname, Duration.ofSeconds(30),"Peter");
-		sendKeys(driver, password, Duration.ofSeconds(30), "Parker");
-		clickOn(driver, forgotAccount, Duration.ofSeconds(30));
+		try
+		{
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.manage().deleteAllCookies();
+			driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+			driver.get("https://www.facebook.com");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try
+		{
+			WebElement firstname = driver.findElement(By.xpath("//*[@id=\"email\"]"));
+			WebElement password = driver.findElement(By.id("pass"));
+			WebElement forgotAccount  = driver.findElement(By.linkText("Forgotten password?"));
+
+			sendKeys(driver, firstname, Duration.ofSeconds(30),"Peter");
+			sendKeys(driver, password, Duration.ofSeconds(30), "Parker");
+			clickOn(driver, forgotAccount, Duration.ofSeconds(30));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
 	public static void clickOn(WebDriver driver1, WebElement element, Duration timeout) {
 		new WebDriverWait(driver1, timeout).until(ExpectedConditions.elementToBeClickable(element));
 		element.click();
-		
 	}
 
 	public static void sendKeys(WebDriver driver1, WebElement element, Duration i, String value) {
@@ -43,12 +52,16 @@ public class SeleniumWaits {
 	@Test
 	public void implicitWaitExercise()
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Katan\\OneDrive\\Desktop\\MyAutomation\\Selenium\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://demoqa.com/dynamic-properties");
-
-		driver.findElement(By.id("visibleAfter")).click();
-		driver.close();
+		try
+		{
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.get("https://demoqa.com/dynamic-properties");
+			driver.findElement(By.id("visibleAfter")).click();
+			driver.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
