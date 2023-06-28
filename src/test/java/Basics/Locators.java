e9a3a534bd4645c3c080c7a970c3d8d0375e2906
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-28 09:30:55 +0200

    Refactored Locators class

--- src/test/java/Basics/Locators.java
+++ src/test/java/Basics/Locators.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class locators {
+public class Locators {
 
 	public static void main(String[] args) throws InterruptedException {
 		try
