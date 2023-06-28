db64bd8af17b3082cdabbcc3faf15e3bf0b17645
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-28 09:30:26 +0200

    Refactored DragAndDrop class

--- src/test/java/Basics/DragAndDrop.java
+++ src/test/java/Basics/DragAndDrop.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class dragAndDrop {
+public class DragAndDrop {
 
 	public static void main(String[] args) throws InterruptedException {
 		try
